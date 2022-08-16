1b08a313ac17db93ce529681d421c029769c1057
zhuBoDian <379837872@qq.com>
2022-08-16 17:21:27 +0800

    bidirectional rnn

--- main_rnn.py
+++ main_rnn.py
@@ -160,13 +160,35 @@ def main_deep_rnn():
     train_rnn(model, train_iter, vocab, lr, num_epochs, device)
 
 
+def main_bi_rnn():
+    print('开始训练bidirectional rnn（错误实例，用于不可预测未来）'.center(100, '*'))
+
+    # 加载数据
+    batch_size, num_steps, device = 32, 35, utils.try_gpu()
+    train_iter, vocab = utils.load_data_time_machine(batch_size, num_steps)
+
+    # 通过设置“bidirective=True”来定义双向LSTM模型
+    vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
+    num_inputs = vocab_size
+    lstm_layer = nn.LSTM(num_inputs, num_hiddens, num_layers, bidirectional=True)
+    model = my_net.MyRNNModel(lstm_layer, len(vocab))
+    model = model.to(device)
+
+    # 训练模型
+    num_epochs, lr = 500, 1
+    train_rnn(model, train_iter, vocab, lr, num_epochs, device)
+
+
+
+
 if __name__ == '__main__':
     torch.manual_seed(42)
 
     # main_my_rnn()
     # main_my_gru()
     # main_my_lstm()
-    main_deep_rnn()
+    # main_deep_rnn()
+    main_bi_rnn()
 
 
 
